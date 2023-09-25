13f04555a02a6186ea5b3565a07eec0d24071c58
Jaimin1304 <jiaming2001_new@outlook.com>
2023-09-26 02:26:59 +1000

    init settings

---
+++ Assets/Scripts/UI/SettingsController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+    public GameObject GeneralPanel;
+    public GameObject SkywayPanel;
+    public GameObject EnvironmentPanel;
+    public GameObject DronePanel;
+
+    void Start()
+    {
+        ShowGeneralPanel(); // Show the General panel by default
+    }
+
+    public void ShowGeneralPanel()
+    {
+        GeneralPanel.SetActive(true);
+        SkywayPanel.SetActive(false);
+        EnvironmentPanel.SetActive(false);
+        DronePanel.SetActive(false);
+    }
+
+    public void ShowSkywayPanel()
+    {
+        GeneralPanel.SetActive(false);
+        SkywayPanel.SetActive(true);
+        EnvironmentPanel.SetActive(false);
+        DronePanel.SetActive(false);
+    }
+
+    public void ShowEnvironmentPanel()
+    {
+        GeneralPanel.SetActive(false);
+        SkywayPanel.SetActive(false);
+        EnvironmentPanel.SetActive(true);
+        DronePanel.SetActive(false);
+    }
+
+    public void ShowDronePanel()
+    {
+        GeneralPanel.SetActive(false);
+        SkywayPanel.SetActive(false);
+        EnvironmentPanel.SetActive(false);
+        DronePanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        Debug.Log("close settings");
+    }
+}
